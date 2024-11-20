8baf9955a581c076be758ca80f01d4335d29b098
reloaddev <henrik.pruess@posteo.de>
2024-11-20 10:40:28 +0100

    highlight country on hover

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 from map import get_map_figure
 import dash_bootstrap_components as dbc
 
@@ -10,11 +10,23 @@ app.layout = dbc.Container(
     html.Div(children='International Students in Europe'),
     html.Div([
         dcc.Graph(
+            id='map-graph',
             figure=get_map_figure(),
             style={"width": "100%", "height": "100%"}
         )
     ])
 ])
+
+@app.callback(
+    Output('map-graph', 'figure'),
+    Input('map-graph', 'hoverData')
+)
+def highlight_country(hoverData):
+    if hoverData is not None:
+        country_id = hoverData['points'][0]['location']
+    else:
+        country_id = None
+    return get_map_figure(highlighted_country=country_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- map.py
+++ map.py
@@ -1,3 +1,4 @@
+from dash import html, Input, Output
 import pandas as pd
 import plotly.express as px
 import json
@@ -8,14 +9,15 @@ with open("europe.geojson") as f:
 country_ids = [feature["properties"]["FIPS"] for feature in europe_geojson["features"]]
 df = pd.DataFrame({"country_id": country_ids})
 
-def get_map_figure():
+def get_map_figure(highlighted_country=None):
+    df['highlight'] = df['country_id'].apply(lambda x: 1 if x == highlighted_country else 0)
     fig = px.choropleth(
         df,
         geojson=europe_geojson,
         featureidkey="properties.FIPS",
-        color="country_id",
+        color="highlight",
         locations="country_id",
-        range_color=[0, 100],
+        range_color=[0, 1],
         fitbounds="locations",
         projection="mercator",
         scope="europe"
@@ -23,7 +25,8 @@ def get_map_figure():
     fig.update_layout(
         height=1000,  # Increase height to make the map larger
         width=2000,  # Increase width for larger display
-        coloraxis_showscale=False  # Hide the color scale
+        coloraxis_showscale=False,  # Hide the color scale
+        dragmode=False
     )
     return fig
 
